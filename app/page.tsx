d436140ed7ffd2bf5937489a53171dfb755f5ce5
TufanKeser <021270079tufan@gmail.com>
2025-01-11 15:59:41 +0300

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => 
    (
     <>
-    <h1 className="h1-bold">tufan</h1>
-    <h1 className="h1-xl">keser</h1>
+    <h1 className="h1-bold">tufantufantufantufan</h1>
+    <h1 className="h1-bold font-space-grotesk">keserkeserkeser</h1>
     </>
   )
 
